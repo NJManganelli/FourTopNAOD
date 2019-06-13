2dcb20716a4f73e800f104570dd3f97f0ff0cd68
Nicholas Manganelli <nmang001@ucr.edu>
2019-06-13 13:23:27 +0200

    Added subprocess histogramming for qq and gg

--- Kai/run/testTopSystemPtv2.py
+++ Kai/run/testTopSystemPtv2.py
@@ -44,6 +44,8 @@ class TopSystemPt(Module):
     def beginJob(self,histFile=None,histDirName=None):
         Module.beginJob(self,histFile,histDirName)
         if histFile and histDirName:
+            self.h_proc=ROOT.TH1I('h_proc', 'Initial pp interacting partons; interaction type; Events', 1, 0, 1)
+            self.addObject(self.h_proc)
             self.h_bfcount=ROOT.TH1I('h_bfcount', 'Count of first b quarks in event; number of b quarks descended from top quarks; frequency', 10, 0, 10)
             self.addObject(self.h_bfcount)
             self.h_bfpcount=ROOT.TH1I('h_bfpcount', 'Count of first b quarks in event after pruning; number of b quarks descended from top quarks with sibling W; frequency', 10, 0, 10)
@@ -52,17 +54,20 @@ class TopSystemPt(Module):
             self.addObject(self.h_Wfcount)
             self.h_TopSystemPt = {}
             self.h_bSystemCorr = {}
-            for i in xrange(4):
-                self.h_TopSystemPt[i]=ROOT.TH3F('h_TopSystemPt_{0:d}'.format(i+1),   
-                                                'Top Sys Pt (R{0:d} t pt); Top_Pt (GeV); Bottom_Pt (GeV); W_Pt (GeV)'.format(i+1), 
-                                                50, 0, 1000, 200, 0, 1000, 200, 0, 1000)
-                                                #200, 0, 1000, 200, 0, 1000, 200, 0, 1000)
-                self.addObject(self.h_TopSystemPt[i])       
-                self.h_bSystemCorr[i]=ROOT.TH3F('h_bSystemCorr_{0:d}'.format(i+1),   
-                                                'b Sys Correlations (R{0:d} t pt);abs(t.eta); Bottom_Pt (GeV); Theta(t, b)'.format(i+1), 
-                                                100, 0, 4, 1000, 0, 1000, 100, 0, math.pi)
-                self.addObject(self.h_bSystemCorr[i])       
-
+            for proc in ["qq", "gg"]:
+                self.h_TopSystemPt[proc] = {}
+                self.h_bSystemCorr[proc] = {}
+                for i in xrange(4):
+                    self.h_TopSystemPt[proc][i]=ROOT.TH3F('h_TopSystemPt_{0:d}_{1:s}'.format(i+1, proc),   
+                                                    'Top Sys Pt (R{0:d} t pt); Top_Pt (GeV); Bottom_Pt (GeV); W_Pt (GeV)'.format(i+1), 
+                                                    50, 0, 1000, 200, 0, 1000, 200, 0, 1000)
+                                                    #200, 0, 1000, 200, 0, 1000, 200, 0, 1000)
+                    self.addObject(self.h_TopSystemPt[proc][i])       
+                    self.h_bSystemCorr[proc][i]=ROOT.TH3F('h_bSystemCorr_{0:d}_{1:s}'.format(i+1, proc),   
+                                                    'b Sys Correlations (R{0:d} t pt);abs(t.eta); Bottom_Pt (GeV); Theta(t, b)'.format(i+1), 
+                                                    100, 0, 4, 1000, 0, 1000, 100, 0, math.pi)
+                    self.addObject(self.h_bSystemCorr[proc][i])       
+    
     def endJob(self):
         if hasattr(self, 'objs') and self.objs != None:
             prevdir = ROOT.gDirectory
@@ -100,6 +105,21 @@ class TopSystemPt(Module):
             raise Exception("Invalid weight option")
 
         gens = Collection(event, "GenPart")
+        # LHEP = Collection(event, "LHEPart")
+        # theLHElist = "The LHE pdgIDs: "
+        # for lhep in LHEP:
+        #     theLHElist += str(lhep.pdgId) + "   "
+        # print(theLHElist)
+        # subproc = "The initial parton IDs:    {0:>5d}   {1:>5d}".format(gens[0].pdgId, gens[1].pdgId)
+        subproc = ""
+        for gen in [gens[0], gens[1]]:
+            if abs(gens[0].pdgId) == 21:
+                subproc += "g"
+            elif abs(gens[0].pdgId) in set([1, 2, 3, 4, 5]):
+                subproc += "q"
+            else:
+                subproc += "X"
+        self.h_proc.Fill(subproc, 1.0)
         bs = [gen for gen in gens if abs(gen.pdgId) == 5]
         Ws = [gen for gen in gens if abs(gen.pdgId) == 24]
         WFirst = [gen for gen in Ws if gen.genPartIdxMother > -1 and abs(gens[gen.genPartIdxMother].pdgId) == 6]#.sort(key=lambda g : gens[g.genPartIdxMother].pt, reverse=True)
@@ -115,7 +135,7 @@ class TopSystemPt(Module):
             b = bFirst[i]
             W = WFirst[i]
             t = gens[bFirst[i].genPartIdxMother]
-            self.h_TopSystemPt[i].Fill(t.pt, b.pt, W.pt, weight)
+            self.h_TopSystemPt[subproc][i].Fill(t.pt, b.pt, W.pt, weight)
             # stats = ""
             # for flag, bits in self.bits.iteritems():
             #     if flag not in ['fromHardProcessBeforeFSR','isFirstCopy','isLastCopy','isLastCopyBeforeFSR']: continue
@@ -130,7 +150,7 @@ class TopSystemPt(Module):
                 cosTheta = 1
             elif cosTheta < -1:
                 cosTheta = -1
-            self.h_bSystemCorr[i].Fill(abs(t.eta - b.eta), b.pt, abs(math.acos(cosTheta)), weight)            
+            self.h_bSystemCorr[subproc][i].Fill(abs(t.eta - b.eta), b.pt, abs(math.acos(cosTheta)), weight)            
             # print(cosTheta)
             #Boost to CoM, measure polar angle between unboosted top and b from CoM -> are there polarization effects?
 
@@ -138,16 +158,16 @@ class TopSystemPt(Module):
 Tuples = []
 filesTTTT=["root://cms-xrd-global.cern.ch//store/mc/RunIIFall17NanoAODv4/TTTT_TuneCP5_PSweights_13TeV-amcatnlo-pythia8/NANOAODSIM/PU2017_12Apr2018_Nano14Dec2018_102X_mc2017_realistic_v6-v1/90000/BD738994-6BD2-6D41-9D93-E0AC468497A5.root"]
 # files=["/eos/home-n/nmangane/AODStorage/TestingSamples/TTTT_TuneCP5_PSweights_102X.root"]
-hNameTTTT="TopSysPtTTTT.root"
+hNameTTTT="TopSysPtTTTTv4.root"
 # hNameTTTTw="TopSysPtTTTTw.root"
 # hNameTTTTabsw="TopSysPtTTTTabsw.root"
-Tuples.append((filesTTTT, hNameTTTT, 0))
+Tuples.append((filesTTTT, hNameTTTT, 0)) #Central test configuration, no weights
 # Tuples.append((filesTTTT, hNameTTTTw, 1))
 # Tuples.append((filesTTTT, hNameTTTTabsw, 2))
 
 filesTT=["root://cms-xrd-global.cern.ch//store/mc/RunIIFall17NanoAODv4/TTTo2L2Nu_TuneCP5_PSweights_13TeV-powheg-pythia8/NANOAODSIM/PU2017_12Apr2018_Nano14Dec2018_new_pmx_102X_mc2017_realistic_v6-v1/80000/FB2C8D48-139E-7647-90C2-1CF1767DB0A1.root"]
-hNameTT="TopSysPtTT.root"
-Tuples.append((filesTT, hNameTT, 0))
+hNameTT="TopSysPtTTv4.root"
+# Tuples.append((filesTT, hNameTT, 0))
 # filesTTGF=["root://cms-xrd-global.cern.ch//store/mc/RunIIFall17NanoAODv4/TTTo2L2Nu_HT500Njet7_TuneCP5_PSweights_13TeV-powheg-pythia8/NANOAODSIM/PU2017_12Apr2018_Nano14Dec2018_102X_mc2017_realistic_v6-v1/90000/E565691C-17D4-6046-865E-8393F1FE0414.root"]
 # hNameTTGF="TopSysPtTTGF.root"
 # hNameTTGFw="TopSysPtTTGFw.root"
@@ -155,6 +175,10 @@ Tuples.append((filesTT, hNameTT, 0))
 # Tuples.append((filesTTGF, hNameTTGF, 0))
 # Tuples.append((filesTTGF, hNameTTGFw, 1))
 # Tuples.append((filesTTGF, hNameTTGFabsw, 2))
+
+filesTT_MG=["root://cms-xrd-global.cern.ch//store/mc/RunIIFall17NanoAODv4/TTJets_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/PU2017_12Apr2018_Nano14Dec2018_102X_mc2017_realistic_v6-v1/90000/79097697-485B-9542-8E6B-43A747EA7F4B.root"]
+hNameTT_MG="TopSysPtTT_MGv4.root"
+# Tuples.append((filesTT_MG, hNameTT_MG, 0))
 
 
 
@@ -168,6 +192,7 @@ def multiplier(fileList, hName=None, wOption=0):
                         fileList,
                         cut=None,
                         modules=[TopSystemPt(maxevt=800000, wOpt=wOption)],
+                        # modules=[TopSystemPt(maxevt=100, wOpt=wOption)],
                         noOut=True,
                         histFileName=hName,
                         histDirName=hDirName,
