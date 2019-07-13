adf5b713d6ce0886b7e2ed79df1ed6be6d92286f
Nicholas Manganelli <nmang001@ucr.edu>
2019-07-13 11:38:11 +0200

    Fix weight calculation, accounting for cross section in pb and lumi in inverse fb

--- Kai/run/testTopSystemPtv2.py
+++ Kai/run/testTopSystemPtv2.py
@@ -23,6 +23,10 @@ class TopSystemPt(Module):
         self.maxEventsToProcess=maxevt
         self.wOpt = wOpt
         self.wMag = wMag
+        self.nPositiveEvents = 0
+        self.nNegativeEvents = 0
+        # print("Weight option is: {} from {}".format(self.wOpt, wOpt))
+        print("Weight magnitude for file is: {}".format(self.wMag))
         self.bits = {'isPrompt':0b000000000000001,
                      'isDecayedLeptonHadron':0b000000000000010,
                      'isTauDecayProduct':0b000000000000100,
@@ -55,10 +59,14 @@ class TopSystemPt(Module):
             self.h_TopSystemPt = {}
             self.h_bSystemCorr = {}
             self.h_bSystemCorrMom = {}
+            self.h_bSystemAngles = {}
+            self.h_bSystemUncorrectedAngles = {}
             for proc in ["qq", "gg", "All"]:
                 self.h_TopSystemPt[proc] = {}
                 self.h_bSystemCorr[proc] = {}
                 self.h_bSystemCorrMom[proc] = {}
+                self.h_bSystemAngles[proc] = {}
+                self.h_bSystemUncorrectedAngles[proc] = {}
                 for i in xrange(4):
                     self.h_TopSystemPt[proc][i]=ROOT.TH3F('h_TopSystemPt_{0:d}_{1:s}'.format(i+1, proc),   
                                                     'Top Sys Pt (R{0:d} t pt); Top_Pt (GeV); Bottom_Pt (GeV); W_Pt (GeV)'.format(i+1), 
@@ -73,8 +81,17 @@ class TopSystemPt(Module):
                                                              'b Sys Correlations (R{0:d} t pt);|#eta_b|; Bottom_Pt (GeV); Bottom_P()'.format(i+1), 
                                                              100, 0, 4, 250, 0, 500, 250, 0, 500)
                     self.addObject(self.h_bSystemCorrMom[proc][i])       
+                    self.h_bSystemAngles[proc][i]=ROOT.TH3F('h_bSystemAngles_{0:d}_{1:s}'.format(i+1, proc),   
+                                                             'b Sys Angles (R{0:d} t pt);#theta_tb; Bottom_Pt (GeV); #theta_bW'.format(i+1), 
+                                                             100, 0, math.pi, 250, 0, 500, 100, 3.07, math.pi)
+                    self.addObject(self.h_bSystemAngles[proc][i])       
+                    self.h_bSystemUncorrectedAngles[proc][i]=ROOT.TH3F('h_bSystemUncorrectedAngles_{0:d}_{1:s}'.format(i+1, proc),   
+                                                             'b Sys Angles (R{0:d} t pt);#theta_tb; Bottom_Pt (GeV); #theta_bW'.format(i+1), 
+                                                             100, 0, math.pi, 250, 0, 500, 100, 3.07, math.pi)
+                    self.addObject(self.h_bSystemUncorrectedAngles[proc][i])       
     
     def endJob(self):
+        print("nPositiveEvents: {}\t nNegativeEvents: {}".format(self.nPositiveEvents, self.nNegativeEvents))
         if hasattr(self, 'objs') and self.objs != None:
             prevdir = ROOT.gDirectory
             self.dir.cd()
@@ -105,12 +122,14 @@ class TopSystemPt(Module):
             weight = 1.0
         elif self.wOpt == 1:
             weight = math.copysign(self.wMag, getattr(event, "genWeight"))
+            if weight < 0:
+                self.nNegativeEvents += 1
+            else:
+                self.nPositiveEvents += 1
         elif self.wOpt == 2:
             weight = abs(getattr(event, "genWeight"))
         else:
             raise Exception("Invalid weight option")
-
-        weight /= getattr(event, "LHEWeight_originalXWGTUP")
 
         gens = Collection(event, "GenPart")
         # LHEP = Collection(event, "LHEPart")
@@ -151,31 +170,54 @@ class TopSystemPt(Module):
             #     if b.statusFlags & bits:
             #         stats += " " + flag
             b_mass = (t.p4() - W.p4()).M()
+            #Boost to CoM, measure polar angle between unboosted top and b from CoM -> are there polarization effects?
             t4 = t.p4()
             b4 = t.p4() - W.p4()
-            self.h_bSystemCorrMom[subproc][i].Fill(abs(b.eta), b.pt, b4.P(), weight)            
-            self.h_bSystemCorrMom["All"][i].Fill(abs(b.eta), b.pt, b4.P(), weight)            
+            W4 = W.p4()
+            b4Uncorrected = b.p4()
+            self.h_bSystemCorrMom[subproc][i].Fill(abs(b.eta), b.pt, b4.P(), weight)
+            self.h_bSystemCorrMom["All"][i].Fill(abs(b.eta), b.pt, b4.P(), weight)
             b4.Boost(-(t4.BoostVector()))
-            cosTheta = (b4.Px()*t4.Px() + b4.Py()*t4.Py() + b4.Pz()*t4.Pz())/(b4.P() * t4.P())
-            if cosTheta > 1:
-                cosTheta = 1
-            elif cosTheta < -1:
-                cosTheta = -1
-            self.h_bSystemCorr[subproc][i].Fill(abs(t.eta - b.eta), b.pt, abs(math.acos(cosTheta)), weight)            
-            self.h_bSystemCorr["All"][i].Fill(abs(t.eta - b.eta), b.pt, abs(math.acos(cosTheta)), weight)            
-            # print(cosTheta)
-            #Boost to CoM, measure polar angle between unboosted top and b from CoM -> are there polarization effects?
+            b4Uncorrected.Boost(-(t4.BoostVector()))
+            W4.Boost(-(t4.BoostVector()))
+            cosTheta_tb = (b4.Px()*t4.Px() + b4.Py()*t4.Py() + b4.Pz()*t4.Pz())/(b4.P() * t4.P())
+            if cosTheta_tb > 1:
+                cosTheta_tb = 1
+            elif cosTheta_tb < -1:
+                cosTheta_tb = -1
+            cosTheta_tb_uncor = (b4Uncorrected.Px()*t4.Px() + b4Uncorrected.Py()*t4.Py() + b4Uncorrected.Pz()*t4.Pz())/(b4Uncorrected.P() * t4.P())
+            if cosTheta_tb_uncor > 1:
+                cosTheta_tb_uncor = 1
+            elif cosTheta_tb_uncor < -1:
+                cosTheta_tb_uncor = -1
+            cosTheta_Wb = (W4.Px()*b4.Px() + W4.Py()*b4.Py() + W4.Pz()*b4.Pz())/(W4.P() * b4.P())
+            if cosTheta_Wb > 1:
+                cosTheta_Wb = 1
+            elif cosTheta_Wb < -1:
+                cosTheta_Wb = -1
+            cosTheta_Wb_uncor = (W4.Px()*b4Uncorrected.Px() + W4.Py()*b4Uncorrected.Py() + W4.Pz()*b4Uncorrected.Pz())/(W4.P() * b4Uncorrected.P())
+            if cosTheta_Wb_uncor > 1:
+                cosTheta_Wb_uncor = 1
+            elif cosTheta_Wb_uncor < -1:
+                cosTheta_Wb_uncor = -1
 
+            self.h_bSystemCorr[subproc][i].Fill(abs(t.eta - b.eta), b.pt, abs(math.acos(cosTheta_tb)), weight)            
+            self.h_bSystemCorr["All"][i].Fill(abs(t.eta - b.eta), b.pt, abs(math.acos(cosTheta_tb)), weight)            
+            self.h_bSystemAngles[subproc][i].Fill(abs(math.acos(cosTheta_tb)), b.pt, abs(math.acos(cosTheta_Wb)), weight)
+            self.h_bSystemAngles["All"][i].Fill(abs(math.acos(cosTheta_tb)), b.pt, abs(math.acos(cosTheta_Wb)), weight)
+            self.h_bSystemUncorrectedAngles[subproc][i].Fill(abs(math.acos(cosTheta_tb_uncor)), b.pt, abs(math.acos(cosTheta_Wb_uncor)), weight)
+            self.h_bSystemUncorrectedAngles["All"][i].Fill(abs(math.acos(cosTheta_tb_uncor)), b.pt, abs(math.acos(cosTheta_Wb_uncor)), weight)
         return True
+
 Tuples = []
 # filesTTTT=["root://cms-xrd-global.cern.ch//store/mc/RunIIFall17NanoAODv4/TTTT_TuneCP5_PSweights_13TeV-amcatnlo-pythia8/NANOAODSIM/PU2017_12Apr2018_Nano14Dec2018_102X_mc2017_realistic_v6-v1/90000/BD738994-6BD2-6D41-9D93-E0AC468497A5.root"]
 filesTTTT = getFiles(query="dbs:/TTTT_TuneCP5_PSweights_13TeV-amcatnlo-pythia8/RunIIFall17NanoAODv4-PU2017_12Apr2018_Nano14Dec2018_102X_mc2017_realistic_v6-v1/NANOAODSIM",
                      redir="root://cms-xrd-global.cern.ch/")
-weightMagTTTT = 0.012*42/(1561946 - 711982)
+weightMagTTTT = 1000*0.012*42/(1561946 - 711982) #Don't forget the pb -> fb conversion factor, stupid
 # files=["/eos/home-n/nmangane/AODStorage/TestingSamples/TTTT_TuneCP5_PSweights_102X.root"]
-hNameTTTT="TopSysPtTTTTv6.root"
-hNameTTTTw="TopSysPtTTTTv6w.root"
-hNameTTTTabsw="TopSysPtTTTTv6absw.root"
+hNameTTTT="TopSysPtTTTTv7p1.root"
+hNameTTTTw="TopSysPtTTTTv7p1w.root"
+hNameTTTTabsw="TopSysPtTTTTv7p1absw.root"
 # Tuples.append((filesTTTT, hNameTTTT, 0)) #Central test configuration, no weights
 Tuples.append((filesTTTT, hNameTTTTw, 1, weightMagTTTT))
 # Tuples.append((filesTTTT, hNameTTTTabsw, 2))
@@ -183,10 +225,10 @@ Tuples.append((filesTTTT, hNameTTTTw, 1, weightMagTTTT))
 
 filesTT = getFiles(query="dbs:/TTTo2L2Nu_TuneCP5_PSweights_13TeV-powheg-pythia8/RunIIFall17NanoAODv4-PU2017_12Apr2018_Nano14Dec2018_new_pmx_102X_mc2017_realistic_v6-v1/NANOAODSIM",
                      redir="root://cms-xrd-global.cern.ch/")
-weightMagTT = 88.341*42/(68875708 - 280100)
-hNameTT="TopSysPtTTv6.root"
-hNameTTw="TopSysPtTTv6w.root"
-hNameTTabsw="TopSysPtTTv6absw.root"
+weightMagTT = 1000*88.341*42/(68875708 - 280100)
+hNameTT="TopSysPtTTv7p1.root"
+hNameTTw="TopSysPtTTv7p1w.root"
+hNameTTabsw="TopSysPtTTv7p1absw.root"
 # Tuples.append((filesTT, hNameTT, 0))
 Tuples.append((filesTT, hNameTTw, 1, weightMagTT))
 # Tuples.append((filesTT, hNameTTabsw, 2))
@@ -194,24 +236,26 @@ Tuples.append((filesTT, hNameTTw, 1, weightMagTT))
 
 filesTTGF = getFiles(query="dbs:/TTTo2L2Nu_HT500Njet7_TuneCP5_PSweights_13TeV-powheg-pythia8/RunIIFall17NanoAODv4-PU2017_12Apr2018_Nano14Dec2018_102X_mc2017_realistic_v6-v1/NANOAODSIM",
                        redir="root://cms-xrd-global.cern.ch/")
-weightMagTTGF = 1.32512*42/(8415626 - 42597)
-hNameTTGF="TopSysPtTTGF.root"
-hNameTTGFw="TopSysPtTTGFw.root"
-hNameTTGFabsw="TopSysPtTTGFabsw.root"
+weightMagTTGF = 1000*1.32512*42/(8415626 - 42597)
+hNameTTGF="TopSysPtTTGFv7p1.root"
+hNameTTGFw="TopSysPtTTGFv7p1w.root"
+hNameTTGFabsw="TopSysPtTTGFv7p1absw.root"
 # Tuples.append((filesTTGF, hNameTTGF, 0))
-Tuples.append((filesTTGF, hNameTTGFw, 1, weightMagTTFG))
+Tuples.append((filesTTGF, hNameTTGFw, 1, weightMagTTGF))
 # Tuples.append((filesTTGF, hNameTTGFabsw, 2))
 
 # filesTT_MG=["root://cms-xrd-global.cern.ch//store/mc/RunIIFall17NanoAODv4/TTJets_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/PU2017_12Apr2018_Nano14Dec2018_102X_mc2017_realistic_v6-v1/90000/79097697-485B-9542-8E6B-43A747EA7F4B.root"]
-# hNameTT_MG="TopSysPtTT_MGv6.root"
-# hNameTT_MGw="TopSysPtTT_MGv6w.root"
-# hNameTT_MGabsw="TopSysPtTT_MGv5v6absw.root"
+# hNameTT_MG="TopSysPtTT_MGv7p1.root"
+# hNameTT_MGw="TopSysPtTT_MGv7p1w.root"
+# hNameTT_MGabsw="TopSysPtTT_MGv5v7p1absw.root"
 # # Tuples.append((filesTT_MG, hNameTT_MGw, 0))
 # # Tuples.append((filesTT_MG, hNameTT_MGw, 1))
 # # Tuples.append((filesTT_MG, hNameTT_MGabsw, 2))
 
 
 def multiplier(fileList, hName=None, wOption=1, weightMagnitude=1):
+    # print("wOption is: {}".format(wOption))
+    # print("input Weight Magnitude is: {}".format(weightMagnitude))
     if hName == None:
         hDirName = None
     else:
@@ -220,7 +264,7 @@ def multiplier(fileList, hName=None, wOption=1, weightMagnitude=1):
         p=PostProcessor(".",
                         fileList,
                         cut=None,
-                        modules=[TopSystemPt(maxevt=20000, wOpt=wOption, wMag = weightMagnitude)],
+                        modules=[TopSystemPt(maxevt=1500000, wOpt=wOption, wMag = weightMagnitude)],
                         noOut=True,
                         histFileName=hName,
                         histDirName=hDirName,
@@ -229,7 +273,7 @@ def multiplier(fileList, hName=None, wOption=1, weightMagnitude=1):
 
 pList = []
 for tup in Tuples:
-    p = multiprocessing.Process(target=multiplier, args=(tup[0], tup[1], tup[2]))
+    p = multiprocessing.Process(target=multiplier, args=(tup[0], tup[1], tup[2], tup[3]))
     pList.append(p)
     p.start()
 
