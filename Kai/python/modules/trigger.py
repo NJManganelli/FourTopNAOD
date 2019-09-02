b620121742d938ff9767999b1715df27c41fde6c
Nicholas Manganelli <nmang001@ucr.edu>
2019-09-02 17:32:44 +0200

    Tested basic tier and channel selection logic, added filtering of triggers to use for selection and veto. Need to set thresholds, add option to insert branches for selected leptons, etc.

--- Kai/python/modules/trigger.py
+++ Kai/python/modules/trigger.py
@@ -1,7 +1,7 @@
 import collections
 from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
 
-TriggerTuple = collections.namedtuple("TriggerTuple", "trigger era subera tier channel leadThresh subMuThresh")
+TriggerTuple = collections.namedtuple("TriggerTuple", "trigger era subera tier channel leadMuThresh subMuThresh leadElThresh subElThresh")
 
 class Trigger(Module):
     def __init__(self, Trigger):
@@ -32,42 +32,31 @@ class Trigger(Module):
         return False
 
 class TriggerAndSelectionLogic(Module):
-    def __init__(self, era="2017", subera=None, isData="False", TriggerChannel=None, fillHists=False):
+    def __init__(self, era="2013", subera=None, isData=False, TriggerChannel=None, fillHists=False):
+        """ Trigger logic that checks for fired triggers and searches for appropriate objects based on criteria set by fired triggers.
+
+        Era is a string with the year of data taking or corresponding MC sample ("2017", "2018")
+        Subera is a string with the subera of data-taking, only for use in combination with isData=True and TriggerChannel ("B", "E", etc.)
+        isData is a boolean for when it's a data sample, as these are handled differently (trigger exclusivity and tier selection) from Monte Carlo.
+        TriggerChannel is a string with the trigger channel ("ElMu" for e-mu channel/dataset, regardless of which is higher pT, "El" for single-electron channel/dataset).
+        fillHists is a boolean for filling histograms.
+
+        Regarding data, internally there are 'tiers' associated with the trigger tuples. For MC, if the event fires any trigger from any tier, it should be accepted. 
+        For data, given that events can be duplicated across data streams ('SingleMuon' and 'MuonEG'), triggers are divided into tiers. The goal is to only select a data event
+        from the highest available tier of triggers that it fires, and veto that event in appropriate data streams when it corresponds to a lower trigger selection.
+        For example, let an event fire both a single muon trigger (tier 4) and a mu-mu trigger (tier 1), but not an e-mu trigger (tier 0). In the double muon dataset, 
+        the event is selected because it fired the tier 1 trigger in the list (and not the tier 0 triggers). In the single muon dataset, the event is veto'd, 
+        because it fired the tier 1 trigger as well as the tier 4."""
         self.era = era
         self.subera = subera
         self.isData = isData
         #Options: "MuMu", "ElMu", "ElEl", "Mu"
         self.TriggerChannel = TriggerChannel
+        if self.isData and (self.subera == None or self.TriggerChannel == None): 
+            raise ValueError("In TriggerAndSelectionLogic is instantiated with isData, both subera and TriggerChannel must be slected ('B', 'F', 'El', 'ElMu', etc.")
         self.fillHists = fillHists
         #TriggerTuple = collections.namedtuple("TriggerTuple", "trigger era subera tier channel leadThresh subMuThresh")
-        self.TriggerList = [TriggerTuple(trigger="HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ",
-                                         era="2017",
-                                         subera="B",
-                                         tier=0,
-                                         channel="MuMu",
-                                         leadMuThresh=25,
-                                         subMuThresh=15,
-                                         leadElThresh=99999,
-                                         subElThresh=99999),
-                            TriggerTuple(trigger="HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass8_v",
-                                         era="2017",
-                                         subera="CDEF",
-                                         tier=0,
-                                         channel="MuMu",
-                                         leadMuThresh=25,
-                                         subMuThresh=15,
-                                         leadElThresh=99999,
-                                         subElThresh=99999),
-                            TriggerTuple(trigger="HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8_v",
-                                         era="2017",
-                                         subera="CDEF",
-                                         tier=0,
-                                         channel="MuMu",
-                                         leadMuThresh=25,
-                                         subMuThresh=15,
-                                         leadElThresh=99999,
-                                         subElThresh=99999),
-                            TriggerTuple(trigger="HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v",
+        self.TriggerList = [TriggerTuple(trigger="HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v",
                                          era="2017",
                                          subera="BCDEF",
                                          tier=0,
@@ -85,10 +74,37 @@ class TriggerAndSelectionLogic(Module):
                                          subMuThresh=15,
                                          leadElThresh=25,
                                          subElThresh=99999),
+                            TriggerTuple(trigger="HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ",
+                                         era="2017",
+                                         subera="B",
+                                         tier=1,
+                                         channel="MuMu",
+                                         leadMuThresh=25,
+                                         subMuThresh=15,
+                                         leadElThresh=99999,
+                                         subElThresh=99999),
+                            TriggerTuple(trigger="HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass8_v",
+                                         era="2017",
+                                         subera="CDEF",
+                                         tier=1,
+                                         channel="MuMu",
+                                         leadMuThresh=25,
+                                         subMuThresh=15,
+                                         leadElThresh=99999,
+                                         subElThresh=99999),
+                            TriggerTuple(trigger="HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8_v",
+                                         era="2017",
+                                         subera="CDEF",
+                                         tier=1,
+                                         channel="MuMu",
+                                         leadMuThresh=25,
+                                         subMuThresh=15,
+                                         leadElThresh=99999,
+                                         subElThresh=99999),
                             TriggerTuple(trigger="HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v",
                                          era="2017",
                                          subera="BCDEF",
-                                         tier=0,
+                                         tier=2,
                                          channel="ElEl",
                                          leadMuThresh=99999,
                                          subMuThresh=99999,
@@ -97,7 +113,7 @@ class TriggerAndSelectionLogic(Module):
                             TriggerTuple(trigger="HLT_IsoMu24_eta2p1_v",
                                          era="2017",
                                          subera="BCD",
-                                         tier=1,
+                                         tier=3,
                                          channel="Mu",
                                          leadMuThresh=25,
                                          subMuThresh=15,
@@ -106,7 +122,7 @@ class TriggerAndSelectionLogic(Module):
                             TriggerTuple(trigger="HLT_IsoMu27_v",
                                          era="2017",
                                          subera="BCD",
-                                         tier=1,
+                                         tier=3,
                                          channel="Mu",
                                          leadMuThresh=30,
                                          subMuThresh=15,
@@ -115,7 +131,7 @@ class TriggerAndSelectionLogic(Module):
                             TriggerTuple(trigger="HLT_IsoMu27_v",
                                          era="2017",
                                          subera="EF",
-                                         tier=1,
+                                         tier=3,
                                          channel="Mu",
                                          leadMuThresh=30,
                                          subMuThresh=15,
@@ -124,46 +140,37 @@ class TriggerAndSelectionLogic(Module):
                             TriggerTuple(trigger="HLT_Ele35_WPTight_Gsf_v",
                                          era="2017",
                                          subera="BCDEF",
-                                         tier=2,
+                                         tier=4,
                                          channel="El",
                                          leadMuThresh=99999,
                                          subMuThresh=15,
                                          leadElThresh=99999,
                                          subElThresh=15)]
-        self.TrigDict = {"2016": {"Principal": {"MuMu": [],
-                                                "ElMu": [],
-                                                "ElEl": []
-                                                },
-                                  "Fallback": {"Mu": []}
-                              },
-                         "2017": {"Principal": {"MuMu": {"B": ["HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ"],
-                                                         "CDEF": ["HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass8_v",
-                                                                  "HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8_v"]
-                                                         },
-                                                "ElMu": {"BCDEF": ["HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v",
-                                                               "HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ_v"]
-                                                         },
-                                                "ElEl": {"BCDEF": ["HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ_v"]
-                                                         }
-                                                }
-                                  "Fallback": {"Mu": {"BCD": ["HLT_IsoMu24_eta2p1_v",
-                                                                "HLT_IsoMu27_v"],
-                                                      "EF": ["HLT_IsoMu27_v"]
-                                                      }
-                                               }
-                              },
-                         "2018": {"Principal": {"MuMu": {"ABCD": []
-                                                         },
-                                                "ElMu": {"ABCD": []
-                                                         },
-                                                "ElEl": {"ABCD": []
-                                                         }
-                                                }
-                                  "Fallback": {"Mu": {"ABCD": []
-                                                      }
-                                               }
-                              },
-                     }
+        #Tiers and TriggerChannels should have a 1-to-1 or 1-to-multiple correspondence, dependant upon how data streams are arranged. Logic of multiple streams will need to be sorted for 2018, wheres some singleLepton stream is folded into the double lepton stream
+        self.TierToChannelDict = {'2016': {0: [None]},
+                                  '2017': {0: ["ElMu"],
+                                           1: ["MuMu"],
+                                           2: ["ElEl"],
+                                           3: ["Mu"],
+                                           4: ["El"]},
+                                  '2018': {0: [None]}
+                                  }
+        self.Triggers = [trigger for trigger in self.TriggerList if self.era == trigger.era and (self.isData == False or (self.subera in trigger.subera and self.TriggerChannel == trigger.channel))]
+        self.tier = [trigger.tier for trigger in self.Triggers]
+        self.tier.sort(key=lambda i: i, reverse=False)
+        print("Sorted trigger tiers selected are: " + str(self.tier))
+        self.tier = self.tier[0]
+        print("Trigger tier selected is: " + str(self.tier))
+        #Create list of veto triggers for data, where explicit tiers are expected.
+        #For 2017, 0 = ElMu dataset, 1 = MuMu, 2 = ElEl, 3 = Mu(Any), 4 = El(Any). Veto any events that fire any higher trigger, to avoid double counting by using this dataset
+        #For 2018, 0 = ElMu dataset, Rest to be determined!
+        self.vetoTriggers = [trigger for trigger in self.TriggerList if self.isData and trigger.tier < self.tier]
+        print("Selected {} Triggers for usage".format(len(self.Triggers)))
+        for t in self.Triggers:
+            print(t)
+        print("Selected {} Triggers for veto".format(len(self.vetoTriggers)))
+        for t in self.vetoTriggers:
+            print(t)
         
     def beginJob(self, histFile=None,histDirName=None):
         if self.fillHists == False:
@@ -192,14 +199,15 @@ class TriggerAndSelectionLogic(Module):
         #run = getattr(event, "run")
         #evt = getattr(event, "event")
         #lumi = getattr(event, "luminosityBlock")
-        
-        for trig in self.Trigger:
-            # if hasattr(event, trig) and getattr(event, trig):
-            if getattr(event, trig, False):
-                #print(getattr(event, trig))
-                return True
-            #else:
-                #print("No trigger fired")
+        Fired = [trigger for trigger in self.Triggers if getattr(event, trigger.trigger, False)]
+        # for trig in self.Triggers:
+        #     fired = [trigger for 
+        #     if getattr(event, trig, False):
+        #         #print(getattr(event, trig))
+        #         return True
+        #     #else:
+        #         #print("No trigger fired")
+        if len(Fired) > 0: return True
         return False
 
 ############## Trigger information
