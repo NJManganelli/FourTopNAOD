941363d292fcf7d09ec333bf7e995721e49c5616
Nicholas Manganelli <nmang001@ucr.edu>
2019-09-17 19:18:53 +0200

    Add protection for .pyc files in submit_all script (Would also be good to look for corresponding directories and skip those as well, but that would be more difficult in the event the requestName is constructed differently) and test why crab started choking on the btagSFProducer import. The latter remains a mystery, with a periodwhere submission just failed. Currently works again. Such is the crab life

--- Kai/scripts/Nanovisor.py
+++ Kai/scripts/Nanovisor.py
@@ -832,6 +832,7 @@ def get_crab_submit_all_py():
 import os, sys, datetime, subprocess
 from glob import glob
 cfg_files = glob("./crab_cfg_*")
+cfg_files = [f for f in cfg_files if ".pyc" not in f]
 for f in cfg_files:
     cmd = "crab submit -c {0:s} > submission_{1:s}.log".format(f.replace("./", ""), f.replace("crab_cfg_", "").replace(".py","").replace("./", ""))
     # subprocess.Popen(args="print '{}'".format(cmd), shell=True, executable="/bin/zsh", env=dict(os.environ))
@@ -851,4 +852,3 @@ def load_sample_cards(sample_cards):
 
 if __name__ == '__main__':
     main()
-
