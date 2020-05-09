8ea1f484adf596677e8bbb2f92d111c7d533e99e
Nick Manganelli <nicholas.james.manganelli@cern.ch>
2020-05-09 21:03:29 +0200

    Switch categorization of btagging to 0+, 1+, 2+ and no exclusives for the moment

--- RDF/FTAnalyzer_Run301046.py
+++ RDF/FTAnalyzer_Run301046.py
@@ -3027,16 +3027,20 @@ def fillHistos(input_df_or_nodes, splitProcess=False, sampleName=None, channel="
                     
                     #These nodes should apply to any/all L0Nodes
                     filterNodes[processName][decayChannel]["L1Nodes"].append(
-                        ("return true;", "Any number of bTags", processName, decayChannel, None, "nMediumDeep{tag}BAny".format(tag=tagger, bpf=branchpostfix), None))
+                        ("return true;".format(tag=tagger, bpf=branchpostfix), "0+ nMediumDeep{tag}B({bpf})".format(tag=tagger, bpf=branchpostfix),processName, decayChannel, None, "nMediumDeep{tag}B0+".format(tag=tagger, bpf=branchpostfix), None))
+                    filterNodes[processName][decayChannel]["L1Nodes"].append(
+                        ("nMediumDeep{tag}B{bpf} >= 1".format(tag=tagger, bpf=branchpostfix), "1+ nMediumDeep{tag}B({bpf})".format(tag=tagger, bpf=branchpostfix), processName, decayChannel, None, "nMediumDeep{tag}B1+".format(tag=tagger, bpf=branchpostfix), None))
+                    filterNodes[processName][decayChannel]["L1Nodes"].append(
+                        ("nMediumDeep{tag}B{bpf} >= 2".format(tag=tagger, bpf=branchpostfix), "2+ nMediumDeep{tag}B({bpf})".format(tag=tagger, bpf=branchpostfix), processName, decayChannel, None, "nMediumDeep{tag}B2+".format(tag=tagger, bpf=branchpostfix), None))
                     # filterNodes[processName][decayChannel]["L1Nodes"].append(
                     #     ("nMediumDeep{tag}B{bpf} == 0".format(tag=tagger, bpf=branchpostfix), "0 nMediumDeep{tag}B({bpf})".format(tag=tagger, bpf=branchpostfix),
                     #      processName, decayChannel, None, "nMediumDeep{tag}B0".format(tag=tagger, bpf=branchpostfix), None))
                     # filterNodes[processName][decayChannel]["L1Nodes"].append(
                     #     ("nMediumDeep{tag}B{bpf} == 1".format(tag=tagger, bpf=branchpostfix), "1 nMediumDeep{tag}B({bpf})".format(tag=tagger, bpf=branchpostfix),
                     #      processName, decayChannel, None, "nMediumDeep{tag}B1".format(tag=tagger, bpf=branchpostfix), None))
-                    filterNodes[processName][decayChannel]["L1Nodes"].append(
-                        ("nMediumDeep{tag}B{bpf} == 2".format(tag=tagger, bpf=branchpostfix), "2 nMediumDeep{tag}B({bpf})".format(tag=tagger, bpf=branchpostfix),
-                         processName, decayChannel, None, "nMediumDeep{tag}B2".format(tag=tagger, bpf=branchpostfix), None))
+                    # filterNodes[processName][decayChannel]["L1Nodes"].append(
+                    #     ("nMediumDeep{tag}B{bpf} == 2".format(tag=tagger, bpf=branchpostfix), "2 nMediumDeep{tag}B({bpf})".format(tag=tagger, bpf=branchpostfix),
+                    #      processName, decayChannel, None, "nMediumDeep{tag}B2".format(tag=tagger, bpf=branchpostfix), None))
                     # filterNodes[processName][decayChannel]["L1Nodes"].append(
                     #     ("nMediumDeep{tag}B{bpf} == 3".format(tag=tagger, bpf=branchpostfix), "3 nMediumDeep{tag}B({bpf})".format(tag=tagger, bpf=branchpostfix),
                     #      processName, decayChannel, None, "nMediumDeep{tag}B3".format(tag=tagger, bpf=branchpostfix), None))
