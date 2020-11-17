5a31aa3741f11b8167d5b237b3b0375cd397a0da
Nick Manganelli <nicholas.james.manganelli@cern.ch>
2020-11-17 16:59:25 +0100

    Fri13 Campaign added for 2017 and 2018 processing of NanoAOD central corrections

---
+++ Kai/crab/NANOv7_Fri13/2017/ST/crab_cfg_2017_ST_tW-NoFHad.py
@@ -0,0 +1,38 @@
+import os
+from WMCore.Configuration import Configuration
+from CRABClient.UserUtilities import config, getUsernameFromCRIC
+
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = '2017_ST_tW-NoFHad'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+config.section_("JobType")
+config.JobType.allowUndistributedCMSSW = True
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'PSet.py'
+config.JobType.maxMemoryMB = 2000
+config.JobType.maxJobRuntimeMin = 1315
+config.JobType.numCores = 1
+config.JobType.scriptExe = 'crab_script_2017_ST_tW-NoFHad.sh'
+config.JobType.inputFiles = ['crab_script_2017_ST_tW-NoFHad.py', 
+                             os.path.join(os.environ['CMSSW_BASE'],'src/PhysicsTools/NanoAODTools/scripts/haddnano.py'),
+                         ]
+config.JobType.outputFiles = ['hist.root']
+config.JobType.sendPythonFolder  = True
+config.section_("Data")
+config.Data.inputDataset = '/ST_tW_top_5f_NoFullyHadronicDecays_TuneCP5_PSweights_13TeV-powheg-pythia8/RunIIFall17NanoAODv7-PU2017_12Apr2018_Nano02Apr2020_new_pmx_102X_mc2017_realistic_v8-v1/NANOAODSIM'
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'FileBased'
+if config.Data.splitting == 'FileBased':
+    config.Data.unitsPerJob = 1
+    # config.Data.totalUnits = $TOTAL_UNITS
+# config.Data.userInputFiles = []
+
+config.Data.outLFNDirBase = '/store/user/{user}/Fri13'.format(user=getUsernameFromCRIC())
+config.Data.publication = True
+config.Data.outputDatasetTag = 'Fri13'
+config.section_("Site")
+config.Site.storageSite = 'T2_CH_CERN'
+
