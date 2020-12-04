8efa57074857b469d593d1d6b77896b3b0055f84
Nick Manganelli <nicholas.james.manganelli@cern.ch>
2020-12-04 01:37:00 +0100

    auto-format for muon configs

--- Kai/python/data/leptonSF/Muon/formSFConfiguration.py
+++ Kai/python/data/leptonSF/Muon/formSFConfiguration.py
@@ -15,15 +15,21 @@ for fn, f in enumerate(["2016/UL/postVFP/Efficiencies_muon_generalTracks_Z_Run20
                         "2017/non-UL/RunBCDEF_SF_ISO_syst.root",
                         "2018/UL/Efficiencies_muon_generalTracks_Z_Run2018_UL_ISO.root",
                         "2017/UL/Efficiencies_muon_generalTracks_Z_Run2017_UL_ID.root"]):
-    if fn != 10: continue
+    # if fn != 10: continue
     print(f)
     kf = f.split("/")[-1]
     in_file = ROOT.TFile.Open(f, "read")
-    subkeys = [kk.GetName() for kk in in_file.GetListOfKeys()]
-    for subkey in subkeys:
-        # if
+    subkeys = [(kk.GetName(), kk.GetClassName()) for kk in in_file.GetListOfKeys()]
+    for subkey, subkeyclassname in subkeys:
+        if subkeyclassname == 'TDirectoryFile': continue
+        if "efficiencyData" in subkey or "efficiencyMC" in subkey: continue
         h = in_file.Get(subkey)
-        print("subkey: ", subkey)
+        # print("subkey: ", subkey)
+        split = subkey.split("_")
+        if "iso" not in split[1].lower():
+            lutkey = split[1]
+        if "iso" in split[1].lower():
+            lutkey = "_".join([split[1], split[3]])
         output = 'muon_options_'
         if "_syst" in subkey:
             output += 'syst'
@@ -31,7 +37,8 @@ for fn, f in enumerate(["2016/UL/postVFP/Efficiencies_muon_generalTracks_Z_Run20
             output += 'stat'
         else:
             output += 'central'
-        output += '[] = {"' + kf + '", "' + subkey + '", "TH2Lookup", ' + ('"Muon_pt", "Muon_eta"' if 'pt' in h.GetXaxis().GetTitle() else '"Muon_eta", "Muon_pt"') + '};'
+        output += '["' + lutkey + '"] = {"' + kf + '", "' + subkey + '", "TH2Lookup", ' + ('"Muon_pt", "Muon_eta"' if 'pt' in h.GetXaxis().GetTitle() else '"Muon_eta", "Muon_pt"') + '};'
         print(output)
+    print("\n\n")
 
     
