4fcf4ba616eb5b06212eaa1ff9dac5eb49bcfd31
Nick Manganelli <nicholas.james.manganelli@cern.ch>
2020-12-17 13:52:14 +0100

    simple cuts and defines handling, lazy snapshot booking to try to parallelize things without root master's RunGraphs() method https://root-forum.cern.ch/t/making-a-single-rdataframe-from-lots-of-files/42764

--- RDF/scripts/nanoframe.py
+++ RDF/scripts/nanoframe.py
@@ -3,18 +3,42 @@ from FourTopNAOD.RDF.tools.toolbox import getFiles
 import ROOT
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 
+# ROOT.gInterpreter.Declare("""
+#     const UInt_t barWidth = 60;
+#     ULong64_t processed = 0, totalEvents = 0;
+#     std::string progressBar;
+#     std::mutex barMutex; 
+#     auto registerEvents = [](ULong64_t nIncrement) {totalEvents += nIncrement;};
+
+#     ROOT::RDF::RResultPtr<ULong64_t> AddProgressBar(ROOT::RDF::RNode df, int everyN=10000, int totalN=100000) {
+#         registerEvents(totalN);
+#         auto c = df.Count();
+#         c.OnPartialResultSlot(everyN, [everyN] (unsigned int slot, ULong64_t &cnt){
+#             std::lock_guard<std::mutex> l(barMutex);
+#             processed += everyN; //everyN captured by value for this lambda
+#             progressBar = "[";
+#             for(UInt_t i = 0; i < static_cast<UInt_t>(static_cast<Float_t>(processed)/totalEvents*barWidth); ++i){
+#                 progressBar.push_back('|');
+#             }
+#             // escape the '\' when defined in python string
+#             std::cout << "\\r" << std::left << std::setw(barWidth) << progressBar << "] " << processed << "/" << totalEvents << std::flush;
+#         });
+#         return c;
+#     }
+# """)
+def getResults(node):
+    return node.GetValue()
+
 def bookSnapshot(input_df, filename, columnList, lazy=True, treename="Events", mode="RECREATE", compressionAlgo="LZ4", compressionLevel=6, splitLevel=99, debug=False):
     if columnList is None:
         columns = input_df.GetColumnNames()
         # raise RuntimeError("Cannot take empty columnList in bookSnapshot")
-    elif isinstance(columnList, str) or 'vector<string>' in str(type(columnList)):
+    elif isinstance(columnList, str) or "<class cppyy.gbl.std.vector<string>" in str(type(columnList)) or 'vector<string>' in str(type(columnList)):
         columns = columnList #regexp case or vector of strings
     elif isinstance(columnList, list):
         columns = ROOT.std.vector(str)()
         for col in columnList:
             columns.push_back(col)
-    elif "<class cppyy.gbl.std.vector<string>" in str(type(columnList)):
-        columns = columnList
     else:
         raise RuntimeError("Cannot handle columnList of type {} in bookSnapshot".format(type(columnList)))
         
@@ -36,10 +60,10 @@ parser.add_argument('--input', dest='input', action='store', type=str, required=
                     help='input path, in the form accepted by getFiles method')
 parser.add_argument('--nThreads', dest='nThreads', action='store', type=int, default=1, #nargs='?', const=0.4,
                     help='number of threads for implicit multithreading (0 or 1 to disable)')
-# parser.add_argument('--defines', dest='defines', action='store', nargs='*', type=str,
-#                     help='list of new variables with syntax variable_name=variable_definition, where both are valid C++ variable names and code, respectively')
-# parser.add_argument('--filters', dest='filters', action='store', nargs='*', type=str,
-#                     help='list of filters with syntax filter_name=filter_cut, where the former is any text and the latter is valid C++ code')
+parser.add_argument('--define', dest='defines', action='append', type=str, #nargs='*'
+                    help='list of new variables with syntax variable_name::variable_definition, where both are valid C++ variable names and code, respectively')
+parser.add_argument('--filter', dest='filters', action='append', type=str, #nargs='*', 
+                    help='list of filters with syntax filter_name::filter_cut, where the former is any text and the latter is valid C++ code')
 parser.add_argument('--keep', dest='keep', action='store', nargs='*', type=str, default=None,
                     help='list of branch names to keep in output files, does not accept wildcards')
 parser.add_argument('--postfix', dest='postfix', action='store', type=str, default="_Skim",
@@ -55,8 +79,10 @@ args = parser.parse_args()
 
 if args.nThreads > 1:
     print("Enabling multithreading. Event order will not be preserved for final skims.")
-    ROOT.ROOT.EnableImplicitMT(nThreads)
+    ROOT.ROOT.EnableImplicitMT(args.nThreads)
 print(args.input)
+print("filters/cuts: ", args.filters)
+print("defines: ", args.defines)
 if args.input.startswith("dbs:") or args.input.startswith("glob:") or args.input.startswith("list:"):
     fileList = getFiles(query=args.input, outFileName=None)
 else:
@@ -66,15 +92,32 @@ print("nThreads: ", args.nThreads)
 print("keep list: ", args.keep)
 print("postfix: ", args.postfix)
 
-for fn in fileList:
+handles = []
+for fnumber, fn in enumerate(fileList):
     foName = fn.replace(".root", args.postfix + ".root")
+    print("output {}: {}".format(fnumber, foName))
     rdf = ROOT.ROOT.RDataFrame("Events", fn)
     rdfFinal = rdf #Placeholder
+    for define in args.defines:
+        var, defn = define.split("::")
+        rdfFinal = rdfFinal.Define(var, defn)
+    for cut in args.filters:
+        name, defn = cut.split("::")
+        rdfFinal = rdfFinal.Filter(defn, name)
     columnList = [str(c) for c in rdf.GetColumnNames() if str(c) in args.keep] if args.keep is not None else None
     if args.write:
-        print("columns to keep: ", columnList)
-        snaphandle = bookSnapshot(rdfFinal, foName, columnList, lazy=False, treename="Events", mode="RECREATE", compressionAlgo="LZMA", compressionLevel=9, splitLevel=99, debug=False)
+        snaphandle = bookSnapshot(rdfFinal, foName, columnList, lazy=True, treename="Events", mode="RECREATE", compressionAlgo="LZMA", compressionLevel=9, splitLevel=99, debug=False)
+        # booktrigger = ROOT.AddProgressBar(ROOT.RDF.AsRNode(), 
+        #                                   2000, int(metainfo[name]["totalEvents"]))
+        handles.append(snaphandle)
 
+results = list(map(getResults, handles))
+print("results length: ", len(results))
+print("results: ", results)
+
+for fn in fileList:
+    foName = fn.replace(".root", args.postfix + ".root")
+    if args.write:
         #Handle the rest of the trees
         fi = ROOT.TFile.Open(fn, "read")
         treeNames = [str(ll.GetName()) for ll in fi.GetListOfKeys() if ll.GetClassName() in ['TTree']]
@@ -90,6 +133,3 @@ for fn in fileList:
                 treeclone.Write()
         fi.Close()
         fo.Close()
-    
-# rdf = ROOT.ROOT.RDataFrame("Events", 
-keepBranches = args.keep
