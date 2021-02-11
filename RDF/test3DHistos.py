e6fd124fe8dfb5f909bf20f71459594ba19c6cf5
Nick Manganelli <nicholas.james.manganelli@cern.ch>
2021-02-11 02:53:37 +0100

    test3DHistos.py, which also erroneously has jet multiplicity SF derivation code, has seen some updates.

--- RDF/test3DHistos.py
+++ RDF/test3DHistos.py
@@ -178,17 +178,21 @@ processGroups["Data"] = {
     
 # test = group(None, ["2018"], processGroups["ttnobb"].get("Names"), channels=["ElMu"], categories=["HT500"], variable="HT", systematic="nom")
 
-f = ROOT.TFile.Open("/eos/user/n/nmangane/analysis/Jan25_2017/Combine/All/2017___Combined.root", "read")
+# f = ROOT.TFile.Open("/eos/user/n/nmangane/analysis/Jan25_2017/Combine/All/2017___Combined.root", "read")
 # f = ROOT.TFile.Open("/eos/user/n/nmangane/analysis/Jan25_2018/Combine/All/2018___Combined.root", "read")
+# f = ROOT.TFile.Open("/eos/user/n/nmangane/analysis/testPUID_2018/Combine/All/2018___Combined.root", "read")
+f = ROOT.TFile.Open("/eos/user/n/nmangane/analysis/Feb5_PUID_nonJetMult2017/Combine/All/2017___Combined.root", "read")
+# f = ROOT.TFile.Open("/eos/user/n/nmangane/analysis/Feb5_PUID_nonJetMult2018/Combine/All/2018___Combined.root", "read")
 fkeys = [kk.GetName() for kk in f.GetListOfKeys()]
 histos = dict()
 name_format="$ERA___$PROCESS___$CHANNEL___$WINDOW___$CATEGORY$BLIND___$VARIABLE___$SYSTEMATIC"
 erasDict = {"2018": ["2018"], "2017": ["2017"], "RunII": ["2017", "2018"]}
 era = "2017"
+# era = "2018"
 channels = ["ElMu", "MuMu", "ElEl"]
 var = "HT"
-syst = "OSDL_2018_top_pTDown"
-# syst = "nom"
+# syst = "OSDL_2018_top_pTDown"
+syst = "nom"
 for k, v in processGroups.items():
     print(k)
     keys = dict()
@@ -213,10 +217,10 @@ for k, v in processGroups.items():
         print(k, " Blind: ", blind, " Succeeded/Failed to find match: ", len(keys[n]), len(fails))
 
 print("\neras = ", erasDict[era], " channels = ", channels, " syst = ", syst)
-print("nBTag, Data, Data/Bkg, Data/(Sgnl+Bkg), ", ['ttnobb','ttbb','singletop','ttH','ttVJets','ttultrarare','DY', 'tttt'])
+print("nBTag, Data-nonttXBkg/ttXBkg, Data/Bkg, Data/(Sgnl+Bkg), ", ['ttnobb','ttbb','singletop','ttH','ttVJets','ttultrarare','DY', 'tttt'])
 for n in ['4', '5', '6', '7', '8+']:
     print("{}j,{},{:.4f},{:.4f},{:s}".format(n, 
-                                            histos['Data'][n].Integral(),
-                                            histos['Data'][n].Integral()/sum([histos[s][n].Integral() for s in ['ttnobb','ttbb','singletop','ttH','ttVJets','ttultrarare','DY']]),
-                                            histos['Data'][n].Integral()/sum([histos[s][n].Integral() for s in ['ttnobb','ttbb','singletop','ttH','ttVJets','ttultrarare','DY', 'tttt']]),
-                                            ",".join(["{:.4f}".format(histos[s][n].Integral()) for s in ['ttnobb','ttbb','singletop','ttH','ttVJets','ttultrarare','DY', 'tttt']])))
+                                             histos['Data'][n].Integral(),
+                                             (histos['Data'][n].Integral() - sum([histos[s][n].Integral() for s in ['singletop','DY']]))/sum([histos[s][n].Integral() for s in ['ttnobb','ttbb','ttH','ttVJets','ttultrarare']]),
+                                             histos['Data'][n].Integral()/sum([histos[s][n].Integral() for s in ['ttnobb','ttbb','singletop','ttH','ttVJets','ttultrarare','DY', 'tttt']]),
+                                             ",".join(["{:.4f}".format(histos[s][n].Integral()) for s in ['ttnobb','ttbb','singletop','ttH','ttVJets','ttultrarare','DY', 'tttt']])))
