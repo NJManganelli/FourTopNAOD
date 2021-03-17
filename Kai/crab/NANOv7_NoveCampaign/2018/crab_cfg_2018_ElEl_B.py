6457c7a61c425aa7628da45fcc62b9c76e1fef2b
Nick Manganelli <nicholas.james.manganelli@cern.ch>
2021-03-17 18:51:43 +0100

    Add 2018 NoveCampaign submission files.

---
+++ Kai/crab/NANOv7_NoveCampaign/2018/crab_cfg_2018_ElEl_B.py
@@ -0,0 +1,39 @@
+import os
+from WMCore.Configuration import Configuration
+from CRABClient.UserUtilities import config, getUsernameFromCRIC
+
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = '2018_ElEl_B'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+config.section_("JobType")
+config.JobType.allowUndistributedCMSSW = True
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'crab_PSet_2018_ElEl_B.py'
+config.JobType.maxMemoryMB = 3000
+config.JobType.maxJobRuntimeMin = 1315
+config.JobType.numCores = 1
+config.JobType.scriptExe = 'crab_script_2018_ElEl_B.sh'
+config.JobType.inputFiles = ['crab_script_2018_ElEl_B.py', 
+                             os.path.join(os.environ['CMSSW_BASE'],'src/PhysicsTools/NanoAODTools/scripts/haddnano.py'),
+                         ]
+config.JobType.outputFiles = [] #['hist.root']
+config.JobType.sendPythonFolder  = True
+config.section_("Data")
+config.Data.inputDataset = '/EGamma/Run2018B-02Apr2020-v1/NANOAOD'
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'FileBased'
+if config.Data.splitting == 'FileBased':
+    config.Data.unitsPerJob = 1
+    # config.Data.totalUnits = $TOTAL_UNITS
+# config.Data.userInputFiles = []
+
+# config.Data.outLFNDirBase = '/store/user/{user}/NoveCampaign'.format(user=getUsernameFromCRIC())
+config.Data.outLFNDirBase = '/store/group/fourtop/NoveCampaign'
+config.Data.publication = True
+config.Data.outputDatasetTag = 'NoveCampaign'
+config.section_("Site")
+config.Site.storageSite = 'T2_BE_IIHE'
+
---
+++ Kai/crab/NANOv7_NoveCampaign/2018/crab_cfg_2018_tt_DL-GF-TuneCP5up.py
@@ -0,0 +1,39 @@
+import os
+from WMCore.Configuration import Configuration
+from CRABClient.UserUtilities import config, getUsernameFromCRIC
+
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = '2018_tt_DL-GF-TuneCP5up'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+config.section_("JobType")
+config.JobType.allowUndistributedCMSSW = True
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'crab_PSet_2018_tt_DL-GF-TuneCP5up.py'
+config.JobType.maxMemoryMB = 3000
+config.JobType.maxJobRuntimeMin = 1315
+config.JobType.numCores = 1
+config.JobType.scriptExe = 'crab_script_2018_tt_DL-GF-TuneCP5up.sh'
+config.JobType.inputFiles = ['crab_script_2018_tt_DL-GF-TuneCP5up.py', 
+                             os.path.join(os.environ['CMSSW_BASE'],'src/PhysicsTools/NanoAODTools/scripts/haddnano.py'),
+                         ]
+config.JobType.outputFiles = [] #['hist.root']
+config.JobType.sendPythonFolder  = True
+config.section_("Data")
+config.Data.inputDataset = '/TTTo2L2Nu_HT500Njet7_TuneCP5up_13TeV-powheg-pythia8/RunIIAutumn18NanoAODv7-Nano02Apr2020_102X_upgrade2018_realistic_v21-v1/NANOAODSIM'
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'FileBased'
+if config.Data.splitting == 'FileBased':
+    config.Data.unitsPerJob = 1
+    # config.Data.totalUnits = $TOTAL_UNITS
+# config.Data.userInputFiles = []
+
+# config.Data.outLFNDirBase = '/store/user/{user}/NoveCampaign'.format(user=getUsernameFromCRIC())
+config.Data.outLFNDirBase = '/store/group/fourtop/NoveCampaign'
+config.Data.publication = True
+config.Data.outputDatasetTag = 'NoveCampaign'
+config.section_("Site")
+config.Site.storageSite = 'T2_BE_IIHE'
+
