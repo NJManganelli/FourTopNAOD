2dcfc9ca267d3faedab1bf157d0cecc8516d7649
Nick Manganelli <nicholas.james.manganelli@cern.ch>
2021-04-27 21:17:02 +0200

    Commit basic condor environment setup using virtual environment.

---
+++ RDF/basic_condor_setup.sh
@@ -0,0 +1,7 @@
+cd /eos/user/n/nmangane/  ### my CERNbox folder
+
+python3 -m virtualenv myvenv   ### create myvenv environment 
+
+source ./myvenv/bin/activate   ### set up the myvenv 
+
+pip3 install -r requirements.txt   ### install packages that were in your requirements.txt file
