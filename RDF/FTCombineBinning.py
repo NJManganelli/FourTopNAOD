046959c13bba63dc2801eedd13429ae5312e279b
Nick Manganelli <nicholas.james.manganelli@cern.ch>
2021-08-17 17:43:30 +0200

    Factorize out the relative uncertainty binning scheme in FTCombineBinning.py

--- RDF/FTCombineBinning.py
+++ RDF/FTCombineBinning.py
@@ -17,9 +17,50 @@ import ROOT
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 
 ROOT.gROOT.SetBatch(True)
-
+def getRelUncertaintyBinning(entryArray, edgesArray, relUncertainty, includeOverflow=False):
+    start = 1 #Don't include underflow
+    stop = start + 1 #Initialize so that we start with checking the singular bin
+    if includeOverflow:
+        end = len(edgesArray) - 1 #nBinsX + 1 #one less since edgesArray is 1 greater than the entryArray in the same dimension
+    else:
+        end = len(edgesArray) - 2
+    binningArray = []
+    binningArray.append(start)
+    with np.errstate(divide='ignore'):
+        while(stop <= end): #Need quality to end to append the end as a stop in the array
+            while(np.any(1/np.sqrt(np.sum(entryArray[:, start:stop], axis=1)) > relUncertainty)):
+                stop += 1
+                #After the while loop, we should have a good start:stop range for rebinning, unless it failed for the last bin, so merge (start:end)! range
+                if stop > end:
+                    if np.any(1/np.sqrt(entryArray[:, start:end].sum(axis=1)) > relUncertainty):
+                        if len(binningArray) > 1:
+                            #Merge with the previous bin by replacement of the last deduced stop with end
+                            binningArray[-1] = end
+                        else:
+                            #Array has only the histogram lowest edge, looks like we've got a one-bin pony show!
+                            binningArray.append(end)
+                        break
+                    else:
+                        raise RuntimeError("Unhandled logical path")
+            if verbose:
+                print("start: {}    stop: {}     end: {}".format(start, stop, end))
+            #Continue handling the stop > end break from the inner while loop
+            if stop > end:
+                break
+            #Append the stop and increment the start and stop
+            binningArray.append(stop)
+            start = copy.copy(stop)
+            stop = copy.copy(start) + 1
+        if verbose: 
+            print("Binning Array: {}".format(binningArray))
+            print("Numpy Entry Array: {}".format(entryArray))
+    ret = edgesArray[np.array(binningArray, dtype=np.int32)]
+    return ret
+    
 def main(stage, analysisDirectory, channel, era, relUncertainty, jsonInput, outDir, sourceVariable = "HT", HTCut="500", 
-         variableSet="HTOnly", categorySet="5x5", merge="", tagger="DeepJet", systematicsToExclude=[], verbose=False):
+         variableSet="HTOnly", categorySet="5x5", merge="", tagger="DeepJet", systematicsToExclude=[], nEquiprobableBins=None, includeOverflow=False, verbose=False):
+    if nEquiprobableBins:
+        print("\n--nEquiprobableBins {} overrides relUncertainty\n".format(nEquiprobableBins))
     varsOfInterest = [sourceVariable + "Unweighted"]
     erasOfInterest = [era]
     channelsOfInterest = [channel]
@@ -65,7 +106,10 @@ def main(stage, analysisDirectory, channel, era, relUncertainty, jsonInput, outD
     channelWindows = list(set(["___".join(k.split("___")[2:4]) for k in keys]))
     categories = sorted(sorted(list(set([k.split("___")[4] for k in keys])), key=lambda j : j.split("nJet")[-1]), key=lambda j: j.split("nMediumDeep")[-1])
     variables = list(set([k.split("___")[5] for k in keys]))
-    systematics = list(set([k.split("___")[6] for k in keys]) - set(systematicsToExclude))
+    if nEquiprobableBins:
+        systematics = ["nom"]
+    else:
+        systematics = list(set([k.split("___")[6] for k in keys]) - set(systematicsToExclude))
     if verbose:
         print("Eras: {}".format(eras))
         print("Samples: {}".format(samples))
@@ -73,7 +117,10 @@ def main(stage, analysisDirectory, channel, era, relUncertainty, jsonInput, outD
         print("Channel + Z Windows: {}".format(channelWindows))
         print("Categories: {}".format(categories))
         print("Variables: {}".format(variables))
-        print("Systematics: {}".format(systematics))
+        if nEquiprobableBins:
+            print("Systematics: {} [for nEquiprobableBins, reduced to one postfix]".format(systematics))
+        else:
+            print("Systematics: {}".format(systematics))
     print("Looping...")
     for era in eras:
         if verbose:
@@ -116,51 +163,18 @@ def main(stage, analysisDirectory, channel, era, relUncertainty, jsonInput, outD
                                     raise RuntimeError("Key not found: {}".format(thisKey))                                    
                     #Have all the systematic variations that are relevant and adding the histograms together; create empty numpy array
                     entryArray = np.zeros((len(systematics), nBinsX+2), dtype=np.int32)
+                    edgesArray = np.zeros(nBinsX+3, dtype=np.float32)
                     # binningArray.append(0) #Can't rebin the overflow anyway... thanks ROOT
                     for nsyst, systematic in enumerate(systematics):
-                        for nbin in range(0, hists[systematic].GetNbinsX()+2): #Include underflow and overflow for bin numbering clarity                            
+                        lastBin = hists[systematic].GetNbinsX()+2
+                        for nbin in range(0, lastBin): #Include underflow and overflow for bin numbering clarity                            
                             entryArray[nsyst, nbin] = hists[systematic].GetBinContent(nbin)
-                    start = 1 #Don't include underflow
-                    stop = start + 1 #Initialize so that we start with checking the singular bin
-                    end = nBinsX + 1 #Include the overflow bin so we still attain its low edge without special logic
-                    binningArray = []
-                    binningArray.append(start)
-                    with np.errstate(divide='ignore'):
-                        while(stop <= end): #Need quality to end to append the end as a stop in the array
-                            while(np.any(1/np.sqrt(np.sum(entryArray[:, start:stop], axis=1)) > relUncertainty)):
-                                stop += 1
-                                #After the while loop, we should have a good start:stop range for rebinning, unless it failed for the last bin, so merge (start:end)! range
-                                if stop > end:
-                                    if np.any(1/np.sqrt(entryArray[:, start:end].sum(axis=1)) > relUncertainty):
-                                        if len(binningArray) > 1:
-                                            #Merge with the previous bin by replacement of the last deduced stop with end
-                                            binningArray[-1] = end
-                                        else:
-                                            #Array has only the histogram lowest edge, looks like we've got a one-bin pony show!
-                                            binningArray.append(end)
-                                        break
-                                    else:
-                                        raise RuntimeError("Unhandled logical path")
-                            if verbose:
-                                print("start: {}    stop: {}     end: {}".format(start, stop, end))
-                            #Continue handling the stop > end break from the inner while loop
-                            if stop > end:
-                                break
-                            #Append the stop and increment the start and stop
-                            binningArray.append(stop)
-                            start = copy.copy(stop)
-                            stop = copy.copy(start) + 1
-                        if verbose: 
-                            print("Binning Array: {}".format(binningArray))
-                            print("Numpy Entry Array: {}".format(entryArray))
-                    rebinningEdges = []
-                    for binNumber in binningArray:
-                        rebinningEdges.append(list(hists.values())[0].GetBinLowEdge(binNumber))
-                    edgesArray = array.array('d', rebinningEdges)
-                    for systematic in systematics:
-                        rebinnedHists[systematic] = hists[systematic].Rebin(len(edgesArray)-1, hists[systematic].GetName() + "_rebin", edgesArray)
-                        # for binNumber in range(1, rebinnedHists[systematic].GetNbinsX()+1):
-                        #     assert rebinnedHists[systematic].GetBinError(binNumber)/rebinnedHists[systematic].GetBinContent(binNumber) <= relUncertainty, "Rebinning not below threshold in the rebinned hist for '{}' systematic, bin {}, range {} - {}".format(systematic, binNumber, rebinnedHists[systematic].GetBinLowEdge(binNumber), rebinnedHists[systematic].GetBinLowEdge(binNumber) + rebinnedHists[systematic].GetBinWidth(binNumber))
+                            if nsyst == 0:
+                                edgesArray[nbin] = hists[systematic].GetBinLowEdge(nbin)
+                        if nsyst == 0:
+                            edgesArray[lastBin] = hists[systematic].GetBinLowEdge(lastBin)
+                    npEdgesArray = getRelUncertaintyBinning(entryArray, edgesArray, relUncertainty, includeOverflow=includeOverflow)
+                    rebinningEdges = npEdgesArray.tolist()
                 replacements = []
                 if jsonInput:
                     for jkey, jdict in jsonCopy.items():
@@ -210,9 +224,12 @@ if __name__ == '__main__':
     parser.add_argument('--categorySet', '--categorySet', dest='categorySet', action='store',
                         type=str, choices=['5x5', '5x3', '5x1', '2BnJet4p', 'FullyInclusive', 'BackgroundDominant'], default='5x3',
                         help='Variable set to include in filling templates')
+    parser.add_argument('--nEquiprobableBins', dest='nEquiprobableBins', default=None, help='number of equiprobable bins to divide each category into')
     parser.add_argument('--excludeSystematics', '--systematicsToExclude', dest='systematicsToExclude', action='append',
                         default=["OSDL_RunII_hdampUp", "OSDL_RunII_hdampDown", "OSDL_RunII_ueUp", "OSDL_RunII_ueDown"],
                         help='List of systematics to not consider when rebinning, scale variations only, e.g. OSDL_RunII_hdampDown')
+    parser.add_argument('--OF', '--includeOverflow', dest='includeOverflow', action='store_true',
+                        help='Include overflow when determining the binning')
     parser.add_argument('--verbose', dest='verbose', action='store_true',
                         help='Enable more verbose output during actions')
     # parser.add_argument('--era', dest='era', action='store', type=str, default="2017", choices=['2016', '2017', '2018'],
