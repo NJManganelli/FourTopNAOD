bdc0d77dc04f768ec10cc39998b6b1c60dc704ac
Nick Manganelli <nicholas.james.manganelli@cern.ch>
2022-05-15 10:49:50 +0200

    FTDriver - usage: condor_submit FTDriver.sub stage=fill-combine var_set=HTOnly cat_set=3pBnJet4p analysis_dir_base=/eos/user/n/nmangane/analysis/April24_jes_reduced

--- RDF/FTDriver.sh
+++ RDF/FTDriver.sh
@@ -15,7 +15,7 @@ channel=$6
 sample=$7
 var_set=$8
 cat_set=$9
-analysis_dir_base=$10
+analysis_dir_base=${10}
 
 
 export XRDPARALLELEVTLOOP=16 #This might only work in development environments, but should increase the throughput...
@@ -29,8 +29,7 @@ export PYTHONPATH=/afs/cern.ch/user/n/nmangane/.local/lib/python3.8/site-package
 cd ~/Work/CMSSW_10_2_24_patch1/src/FourTopNAOD/RDF
 source ~/Work/CMSSW_10_2_24_patch1/src/FourTopNAOD/RDF/standalone/env_standalone.sh
 
-echo ${job_number} ${stage} ${era} ${channel} ${var_set} ${cat_set} ${sample} >> ${analysis_dir}/condor_touch.log
-echo "Running the analyzer... filling plots"
-echo python FTAnalyzer.py ${stage} --variableSet ${var_set} --categorySet ${cat_set} --analysisDirectory ${analysis_dir_base}_${era} --noAggregate --channel ${channel} --bTagger DeepJet --jetPUId L --include ${sample} --nThreads ${num_threads} --source NANOv7_CorrNov__${channel} --sample_cards '../Kai/python/samplecards/'${era}'_NanoAODv7.yaml' '../Kai/python/samplecards/'${era}'_NanoAODv7_additional.yaml' --systematics_cards '../Kai/python/samplecards/'${era}'_systematics_NanoV7_V8.yaml' --noProgressBar --era ${era} >> ${analysis_dir}/condor_command.log
+echo ${job_number} ${stage} ${era} ${channel} ${var_set} ${cat_set} ${sample} >> ${analysis_dir_base}_${era}/condor_touch.log
+echo Job ${job_number} python FTAnalyzer.py ${stage} --variableSet ${var_set} --categorySet ${cat_set} --analysisDirectory ${analysis_dir_base}_${era} --noAggregate --channel ${channel} --bTagger DeepJet --jetPUId L --include ${sample} --nThreads ${num_threads} --source NANOv7_CorrNov__${channel} --sample_cards '../Kai/python/samplecards/'${era}'_NanoAODv7.yaml' '../Kai/python/samplecards/'${era}'_NanoAODv7_additional.yaml' --systematics_cards '../Kai/python/samplecards/'${era}'_systematics_NanoV7_V8.yaml' --noProgressBar --era ${era} >> ${analysis_dir_base}_${era}/condor_command.log
 
 python FTAnalyzer.py ${stage} --variableSet ${var_set} --categorySet ${cat_set} --analysisDirectory ${analysis_dir_base}_${era} --noAggregate --channel ${channel} --bTagger DeepJet --jetPUId L --include ${sample} --nThreads ${num_threads} --source NANOv7_CorrNov__${channel} --sample_cards '../Kai/python/samplecards/'${era}'_NanoAODv7.yaml' '../Kai/python/samplecards/'${era}'_NanoAODv7_additional.yaml' --systematics_cards '../Kai/python/samplecards/'${era}'_systematics_NanoV7_V8.yaml' --noProgressBar --era ${era}
