3e20e435d4f3f49d31a5d0ba1a0d563618948ef2
Nick Manganelli <nicholas.james.manganelli@cern.ch>
2022-05-16 11:25:47 +0200

    Updated Driver with use of flock for common file writing and support for more arguments

--- RDF/FTDriver.sh
+++ RDF/FTDriver.sh
@@ -16,6 +16,8 @@ sample=$7
 var_set=$8
 cat_set=$9
 analysis_dir_base=${10}
+btagger=${11}
+jetpuid=${12}
 
 
 export XRDPARALLELEVTLOOP=16 #This might only work in development environments, but should increase the throughput...
@@ -24,12 +26,15 @@ source /cvmfs/sft.cern.ch/lcg/views/LCG_102rc1/x86_64-centos7-gcc11-opt/setup.sh
 # export LHAPDF_PATH=/cvmfs/sft.cern.ch/lcg/releases/LCG_100/MCGenerators/lhapdf/6.3.0/x86_64-centos7-gcc8-opt
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$LHAPDF_PATH/lib/
 # export LHAPDF_DATA_PATH=/cvmfs/sft.cern.ch/lcg/external/lhapdfsets/current/:${LHAPDF_PATH}/share/LHAPDF
-export PYTHONPATH=/afs/cern.ch/user/n/nmangane/.local/lib/python3.8/site-packages/:$PYTHONPATH
+export PYTHONPATH=/afs/cern.ch/user/n/nmangane/.local/lib/python3.9/site-packages/:$PYTHONPATH
 
 cd ~/Work/CMSSW_10_2_24_patch1/src/FourTopNAOD/RDF
 source ~/Work/CMSSW_10_2_24_patch1/src/FourTopNAOD/RDF/standalone/env_standalone.sh
 
-echo ${job_number} ${stage} ${era} ${channel} ${var_set} ${cat_set} ${sample} >> ${analysis_dir_base}_${era}/condor_touch.log
-echo Job ${job_number} python FTAnalyzer.py ${stage} --variableSet ${var_set} --categorySet ${cat_set} --analysisDirectory ${analysis_dir_base}_${era} --noAggregate --channel ${channel} --bTagger DeepJet --jetPUId L --include ${sample} --nThreads ${num_threads} --source NANOv7_CorrNov__${channel} --sample_cards '../Kai/python/samplecards/'${era}'_NanoAODv7.yaml' '../Kai/python/samplecards/'${era}'_NanoAODv7_additional.yaml' --systematics_cards '../Kai/python/samplecards/'${era}'_systematics_NanoV7_V8.yaml' --noProgressBar --era ${era} >> ${analysis_dir_base}_${era}/condor_command.log
+flock -w 120 ${analysis_dir_base}_${era}/${sample}_${channel}_touch.log echo ${job_number} ${stage} ${era} ${channel} ${var_set} ${cat_set} ${sample} >> ${analysis_dir_base}_${era}/${sample}_${channel}_touch.log
 
-python FTAnalyzer.py ${stage} --variableSet ${var_set} --categorySet ${cat_set} --analysisDirectory ${analysis_dir_base}_${era} --noAggregate --channel ${channel} --bTagger DeepJet --jetPUId L --include ${sample} --nThreads ${num_threads} --source NANOv7_CorrNov__${channel} --sample_cards '../Kai/python/samplecards/'${era}'_NanoAODv7.yaml' '../Kai/python/samplecards/'${era}'_NanoAODv7_additional.yaml' --systematics_cards '../Kai/python/samplecards/'${era}'_systematics_NanoV7_V8.yaml' --noProgressBar --era ${era}
+flock -w 120 ${analysis_dir_base}_${era}/${sample}_${channel}_command.log echo Job ${job_number} python FTAnalyzer.py ${stage} --variableSet ${var_set} --categorySet ${cat_set} --analysisDirectory ${analysis_dir_base}_${era} --noAggregate --channel ${channel} --bTagger ${btagger} --jetPUId ${jetpuid} --include ${sample} --nThreads ${num_threads} --source NANOv7_CorrNov__${channel} --sample_cards '../Kai/python/samplecards/'${era}'_NanoAODv7.yaml' '../Kai/python/samplecards/'${era}'_NanoAODv7_additional.yaml' --systematics_cards '../Kai/python/samplecards/'${era}'_systematics_NanoV7_V8.yaml' --noProgressBar --era ${era} >> ${analysis_dir_base}_${era}/${sample}_${channel}_command.log
+
+# (echo Job ${job_number} python FTAnalyzer.py ${stage} --variableSet ${var_set} --categorySet ${cat_set} --analysisDirectory ${analysis_dir_base}_${era} --noAggregate --channel ${channel} --bTagger ${btagger} --jetPUId ${jetpuid} --include ${sample} --nThreads ${num_threads} --source NANOv7_CorrNov__${channel} --sample_cards '../Kai/python/samplecards/'${era}'_NanoAODv7.yaml' '../Kai/python/samplecards/'${era}'_NanoAODv7_additional.yaml' --systematics_cards '../Kai/python/samplecards/'${era}'_systematics_NanoV7_V8.yaml' --noProgressBar --era ${era} >> ${analysis_dir_base}_${era}/${sample}_${channel}_command.log) || (echo FAILED TO WRITE TO JOB LOG: Job ${job_number} python FTAnalyzer.py ${stage} --variableSet ${var_set} --categorySet ${cat_set} --analysisDirectory ${analysis_dir_base}_${era} --noAggregate --channel ${channel} --bTagger ${btagger} --jetPUId ${jetpuid} --include ${sample} --nThreads ${num_threads} --source NANOv7_CorrNov__${channel} --sample_cards '../Kai/python/samplecards/'${era}'_NanoAODv7.yaml' '../Kai/python/samplecards/'${era}'_NanoAODv7_additional.yaml' --systematics_cards '../Kai/python/samplecards/'${era}'_systematics_NanoV7_V8.yaml' --noProgressBar --era ${era})
+
+python FTAnalyzer.py ${stage} --variableSet ${var_set} --categorySet ${cat_set} --analysisDirectory ${analysis_dir_base}_${era} --noAggregate --channel ${channel} --bTagger ${btagger} --jetPUId ${jetpuid} --include ${sample} --nThreads ${num_threads} --source NANOv7_CorrNov__${channel} --sample_cards '../Kai/python/samplecards/'${era}'_NanoAODv7.yaml' '../Kai/python/samplecards/'${era}'_NanoAODv7_additional.yaml' --systematics_cards '../Kai/python/samplecards/'${era}'_systematics_NanoV7_V8.yaml' --noProgressBar --era ${era}
